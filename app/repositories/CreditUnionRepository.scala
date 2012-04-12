f15e0131a6863fc64efede978435061e5a5a3c88
Praful <ptodkar@gmail.com>
2012-04-12 14:10:14 -0400

    added credit union repository and model; added sbteclipse plugin; included neo4j in the project

---
+++ app/repositories/CreditUnionRepository.scala
@@ -0,0 +1,9 @@
+package repositories
+
+import org.neo4j.kernel.EmbeddedGraphDatabase
+import org.neo4j.graphdb._
+import collection.JavaConversions._
+
+class CreditUnionRepository {
+
+}
--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,17 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+    // Add your project dependencies here,
+    "org.neo4j.app" % "neo4j-server" % "1.6",
+    "org.neo4j.app" % "neo4j-server" % "1.6" classifier "static-web",
+    "com.sun.jersey" % "jersey-core" % "1.9"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+    // Add your own project settings here      
+    resolvers ++= Seq(
+      "tinkerprop" at "http://tinkerpop.com/maven2",
+      "neo4j-public-repository" at "http://m2.neo4j.org/releases")
     )
 
 }
